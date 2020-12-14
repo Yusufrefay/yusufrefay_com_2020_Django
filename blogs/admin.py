89c25e5d0d148daf46613eb9aef6e56f9bda1bdf
Yusuf Refay <yusufrefay@outlook.com>
2020-12-15 01:43:52 +0200

    Make Modules

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import blog
 
-# Register your models here.
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title','update_date')
+    ordering = ('title',)
+    search_fields = ('title',) 
+
+admin.site.register(blog, BlogAdmin)
