c3d2ce4d2290620770a3f5487a9d353fffd6242e
Yusuf Refay <yusufrefay@outlook.com>
2020-12-17 18:52:45 +0200

    Add blog section to static Design

--- blogs/urls.py
+++ blogs/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    # path('', views.index,{'blogname': ''}, name='home'),
+    # path('<str:blogname>', views.index, name='index'),
 ]
