d6cdf230075ef014d77b1f07fada0d965a5ac523
Yusuf Refay <yusufrefay@outlook.com>
2021-01-10 21:59:14 +0200

    Working?

--- yusufrefay_com_2020/settings.py
+++ yusufrefay_com_2020/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-#Local Lines
+#is it Working?
 
 STATIC_URL = '/static/'
 
@@ -134,4 +134,3 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
-#Production Lines
