8807163181432107d91dc6641097275574c8d0a4
Yusuf Refay <yusufrefay@outlook.com>
2021-01-11 00:00:54 +0200

    Turning  off Debuging

--- yusufrefay_com_2020/settings.py
+++ yusufrefay_com_2020/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '0)@5cp&9vlq^rb#5q@b5x1i^1t$qlpdc#5n)m=7a(k=(8(5mks'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = os.environ.get('DEBUG_STATUS')
-DEBUG = 'True'
+DEBUG = 'False'
 
 ALLOWED_HOSTS = ['yusufrefay.com', 'www.yusufrefay.com']
 
